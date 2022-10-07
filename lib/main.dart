decdd2ea222c85f9cc3bdcc727ce914dc3a016e7
atitaya <6203009@gmail.com>
2022-10-07 15:09:28 +0700

    update project 02

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "My App",
       home: MyHomePage(),
-      theme: ThemeData(primarySwatch: Colors.pink),
+      theme: ThemeData(primarySwatch: Colors.purple),
     );
   }
 }
@@ -28,35 +28,145 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePage extends State<MyHomePage> {
 //กลุ่มข้อมูล
-  List<FoodMenu> menu = [
-    FoodMenu("ปลาเผา", "500", "assets/images/01.jpg"),
-    FoodMenu("หมูย่าง", "600", "assets/images/02.jpg"),
-    FoodMenu("กุ้งทอด", "6000", "assets/images/03.jpg"),
-    FoodMenu("หมึกย่าง", "5000", "assets/images/04.jpg")
-  ];
+  // List<FoodMenu> menu = [
+  //   FoodMenu("ปลาเผา", "500", "assets/images/01.jpg"),
+  //   FoodMenu("หมูย่าง", "600", "assets/images/02.jpg"),
+  //   FoodMenu("กุ้งทอด", "6000", "assets/images/03.jpg"),
+  //   FoodMenu("หมึกย่าง", "5000", "assets/images/04.jpg")
+  // ];
 
 //แสดงผลข้อมูล
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("เลือกเมนูอาหาร"),
+          title: Text(
+            "บัญชีของฉัน",
+            style: TextStyle(
+                fontSize: 25, color: Colors.white, fontWeight: FontWeight.bold),
+          ),
         ),
-        body: ListView.builder(
-            itemCount: menu.length,
-            itemBuilder: (BuildContext context, int index) {
-              FoodMenu food = menu[index];
-              return ListTile(
-                leading: Image.asset(food.img),
-                title: Text(
-                  food.name,
-                  style: TextStyle(fontSize: 30),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(10)),
+                height: 130,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "ยอดคงเหลือ",
+                      style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Expanded(
+                        child: Text(
+                      "10000",
+                      style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.right,
+                    ))
+                  ],
                 ),
-                subtitle: Text(
-                  "ราคา" + food.price + "บาท",
-                  style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(height: 10,),
+                Container(
+                padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                    color: Colors.lightBlue,
+                    borderRadius: BorderRadius.circular(10)),
+                height: 100,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "รายรับ",
+                      style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Expanded(
+                        child: Text(
+                      "10000",
+                      style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.right,
+                    ))
+                  ],
                 ),
-              );
-            }));
+              ),
+                   SizedBox(height: 10,),
+                Container(
+                padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(10)),
+                height: 100,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "รายจ่าย",
+                      style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Expanded(
+                        child: Text(
+                      "10000",
+                      style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.right,
+                    ))
+                  ],
+                ),
+              ),
+               SizedBox(height: 10,),
+                Container(
+                padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                    color: Colors.lightBlue,
+                    borderRadius: BorderRadius.circular(10)),
+                height: 100,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "ค่าค้างชำระ",
+                      style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Expanded(
+                        child: Text(
+                      "10000",
+                      style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.right,
+                    ))
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
